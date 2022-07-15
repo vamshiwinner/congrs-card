2a47ddc364f78a892cd5c1b6fc485f2fa2a60f82
vamshiwinner <vamshikrishna7999@gmail.com>
2022-07-15 04:50:38 +0000

    cngrs card

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="bg-container">
+    <h1>Congratultions</h1>
+    <div className="bg-container2">
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+      />
+      <h1>kiran V</h1>
+      <p>Vishnu Institute of science and technology</p>
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
